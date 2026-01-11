9baef5bc6179503ed78d6ca2cfe25c71313802cb
Toukir Rahman <to.toukir.rahman@gmail.com>
2026-01-11 17:57:16 +0600

    no message

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,12 +5,12 @@ import PageHeader from "@/components/layout/PageHeader"
 
 export default function DashboardPage() {
   return (
-    <div className="min-h-screen ml-20 p-10 space-y-8 bg-[#E4E2DE] dark:bg-slate-950">
+    <div className="min-h-screen space-y-8 bg-[#E4E2DE] dark:bg-slate-950">
       <PageHeader description="Welcome to your dashboard. Here you can see quick stats and navigate to other sections."/>
 
       {/* Example cards */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <div className="p-4 bg-slate-800 rounded-lg shadow">
+        <div className="p-4 bg-slate-800 rounded-lg shadow min-h-[2000px]">
           <h2 className="text-lg font-semibold text-white">Users</h2>
           <p className="text-slate-400">Manage all your users</p>
         </div>
