9a0b8f10c4dea030431d3dee7eabed7a8e65d61b
Toukir Rahman <to.toukir.rahman@gmail.com>
2026-01-20 17:38:25 +0600

    no message

--- src/components/layout/MenuBar.tsx
+++ src/components/layout/MenuBar.tsx
@@ -8,9 +8,9 @@ import { cn } from "@/lib/utils"
 import { useTheme } from "@/hooks/useTheme"
 import {
   InputGroup,
-  InputGroupAddon,
   InputGroupInput,
 } from "@/components/ui/input-group"
+import { Icon } from '@iconify/react'
 import {
   LayoutDashboard,
   Bell,
@@ -21,8 +21,7 @@ import {
   Briefcase,
   Users, 
   Sun, 
-  Moon,
-  SearchIcon
+  Moon
 } from "lucide-react"
 import PageHeader from "./PageHeader"
 
@@ -40,46 +39,46 @@ export default function MenuBar() {
   const pathname = usePathname()
   const { theme, toggleTheme } = useTheme()
   return (
-    <aside className="menuBar-side">
-      <div className="mb-8 text-lg font-bold text-white">
-        ⚡
+    <aside>
+      <div className="menuBar-side">
+        <div className="mb-8 text-lg font-bold text-white">
+          ⚡
+        </div>
+        <nav className="flex flex-col gap-1 flex-1">
+          {navItems.map(({ href, icon: Icon, label }) => {
+            const isActive = pathname === href
+            return (
+              <Link key={href} href={href} title={label} className={cn('navMenus', isActive ? "active" : "")}>
+                <Icon className="h-[16px] w-[16px]" />
+                <span>{label}</span>
+              </Link>
+            )
+          })}
+        </nav>
+        <div className="flex flex-col gap-1 pt-6">
+          <button title="User profile" className="navMenus overflow-hidden">
+            <Image className="overflow-hidden object-cover" src="/images/user.webp" alt="User" width={56} height={56}/>
+          </button>
+          <button title="Logout" onClick={() => { localStorage.removeItem("token"); window.location.href = "/sign-in"}} className="navMenus logoutMenu">
+            <LogOut className="h-5 w-5" />
+          </button>
+        </div>
       </div>
-      <nav className="flex flex-col gap-1 flex-1">
-        {navItems.map(({ href, icon: Icon, label }) => {
-          const isActive = pathname === href
-          return (
-            <Link key={href} href={href} title={label} className={cn('navMenus', isActive ? "active" : "")}>
-              <Icon className="h-[16px] w-[16px]" />
-              <span>{label}</span>
-            </Link>
-          )
-        })}
-      </nav>
-      <div className="flex flex-col gap-1 pt-6">
-        <button title="User profile" className="navMenus overflow-hidden">
-          <Image className="overflow-hidden object-cover" src="/images/user.webp" alt="User" width={56} height={56}/>
-        </button>
-        <button title="Logout" onClick={() => { localStorage.removeItem("token"); window.location.href = "/sign-in"}} className="navMenus logoutMenu">
-          <LogOut className="h-5 w-5" />
-        </button>
-      </div>
-      <div className="fixed right-0 top-0 flex justify-between gap-1 z-20 w-full p-5 ps-[120px]">
+      <div className="fixed right-0 top-0 flex justify-between gap-1 z-20 w-full p-5 ps-[120px] backdrop-blur-[100px]">
         <PageHeader/>
         <div className="flex gap-1">
-          <InputGroup className="w-[300px] h-[46px] bg-white/80 hover:bg-white dark:bg-slate-700 dark:hover:bg-slate-600 border-0 rounded-[10px]">
-            <InputGroupInput className="w-full text-slate-600 dark:text-white" placeholder="Search..." />
-            <InputGroupAddon>
-              <SearchIcon className="text-slate-900 dark:text-white/80 w-8 h-8" />
-            </InputGroupAddon>
+          <InputGroup className="w-[300px] h-[46px] bg-white/80 hover:bg-white dark:bg-slate-700 dark:hover:bg-slate-600 rounded-[10px] border-0 shadow">
+            <Icon icon="lineicons:search-1" width="24" height="24" className="text-slate-800 dark:text-white ms-3"/>
+            <InputGroupInput className="searchInput" placeholder="Search..." />
           </InputGroup>
           <button onClick={toggleTheme} role="switch" aria-checked={theme === "dark"} title="Toggle theme"
-            className={cn("relative h-[46px] w-[86px] rounded-[10px] transition-colors duration-300", theme === "dark" ? "bg-slate-700" : "bg-white/50")}>
-            <span className={cn("absolute top-[3px] left-[3px] h-[40px] w-[40px] rounded-[7px] bg-white dark:bg-slate-600 shadow-md transition-transform duration-300", theme === "dark" && "translate-x-[40px]")}/>
-            <Sun className="absolute left-3 top-3 h-5 w-5 text-slate-700 dark:text-yellow-400 "/>
-            <Moon className="absolute right-3 top-3 h-5 w-5 text-slate-700 dark:text-white"/>
+            className={cn("relative h-[46px] w-[86px] rounded-[10px] transition-colors duration-300 shadow", theme === "dark" ? "bg-slate-700" : "bg-white/80")}>
+            <span className={cn("absolute top-[4px] left-[4px] h-[38px] w-[38px] rounded-[8px] bg-slate-800 dark:bg-white transition-transform duration-300", theme === "dark" && "translate-x-[40px]")}/>
+            <Sun className="absolute left-3 top-3 h-5 w-5 text-white dark:text-yellow-400 "/>
+            <Moon className="absolute right-3 top-3 h-5 w-5 text-slate-700 dark:text-slate-800"/>
           </button>
           <button title="Notifications" className="relative h-[46px] w-[46px] rounded-[10px] bg-white/80 hover:bg-white dark:bg-slate-700 dark:hover:bg-slate-600 
-          flex items-center justify-center text-slate-500 hover:text-slate-900 dark:text-slate-500 dark:hover:text-white">
+          flex items-center justify-center text-slate-700 hover:text-slate-900 dark:text-slate-300 dark:hover:text-white shadow">
             <Bell className="h-5 w-5"/>
             <div className="absolute top-2 right-2 h-2 w-2 rounded-full bg-red-500"></div>
           </button>
